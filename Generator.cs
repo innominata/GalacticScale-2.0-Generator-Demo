1663976b6a5480ab3d6d16450c0da8759a415972
innominata <inno@xray.cat>
2021-05-07 22:43:37 +1200

    Initial Commit. Very Alpha, will definitely change as GS2 changes.

---
+++ Generator.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections.Generic;
+using BepInEx;
+
+
+namespace GalacticScale.Generators
+{
+    public class DemoGenerator : iConfigurableGenerator
+    {
+        public string Name => "Demo";
+        public string Author => "innominata";
+        public string Description => "Sample Generator that doesn't do a lot";
+        public string Version => "0.0";
+        public string GUID => "space.customizing.generators.demo";
+        public GSGeneratorConfig Config => config; // Return our own generator config when asked, instead of using default config
+        public List<GSOption> Options => options; // Likewise for options
+        public bool DisableStarCountSlider => false;
+        public void Init()
+        {
+            GS2.Log("Initializing Demo Generator"); // Use Galactic Scales Log system for debugging purposes.
+            config.DisableSeedInput = true;
+            config.DisableStarCountSlider = false;
+            config.MaxStarCount = 1024; //1024 is game limit, and already ridiculous. Setting this higher will cause the game to crash.
+            config.MinStarCount = 1;
+            List<string> densityList = new List<string>() { "Densest", "Denser", "Default", "Sparse", "Sparsest" };
+            options.Add(new GSOption("Density", "ComboBox", densityList, DensitySelectCallback, InitializeDensitySelect));
+        }
+        public void Import(GSGenPreferences prefs) //This is called on game start, with the same object that was exported last time
+        {
+            preferences = prefs;
+            SetDensity(prefs.GetInt("Density", 0)); // GSGenPreferences has some helper methods such as a guaranteed integer return, with default.
+        }
+        public GSGenPreferences Export() // Send our custom preferences object to be saved to disk
+        {
+            return preferences;
+        }
+        public void Generate(int starCount) // Redirect to our private generation method
+        {
+            generate(starCount);
+        }
+        //////////////////////////////////////////////////////////////////////
+        ///// All code below here is generator specific
+        //////////////////////////////////////////////////////////////////////
+        private float minStepLength = 2.3f;
+        private float maxStepLength = 3.5f;
+        private float minDistance = 2f;
+        public List<GSOption> options = new List<GSOption>();
+        private GSGenPreferences preferences = new GSGenPreferences();
+        private GSGeneratorConfig config = new GSGeneratorConfig();
+
+        ////////////////////////////
+        /// Handle Settings Elements
+        ////////////////////////////
+        public void InitializeDensitySelect() // UI Element Postfix methods are called once UI elements have been created
+        {
+            options[0].rectTransform.GetComponentInChildren<UIComboBox>().itemIndex = preferences.GetInt("Density",0); // Option objects' rectTransform is set once the UI element has been created
+        }
+        public void DensitySelectCallback(object o) => SetDensity((int)o); // Callback methods are called when the user interacts with the UI element
+
+        //////////////////////////////
+        /// Finally, lets do something
+        //////////////////////////////
+
+        public void SetDensity(int index)
+        {
+            preferences.Set("Density",index);
+            int i = index;
+            switch (i)
+            {
+                case 0: minStepLength = 1.2f; maxStepLength = 1.5f; minDistance = 1.2f; break;
+                case 1: minStepLength = 1.6f; maxStepLength = 2.5f; minDistance = 1.7f; break;
+                case 3: minStepLength = 2.2f; maxStepLength = 5.0f; minDistance = 2.2f; break;
+                case 4: minStepLength = 3.0f; maxStepLength = 7.0f; minDistance = 3.0f; break;
+                default: minStepLength = 2f; maxStepLength = 3.5f; minDistance = 2.3f; break;
+            }
+        }
+
+        public void generate(int starCount)
+        {
+            SetDensity(preferences.GetInt("Density",2));
+
+            //Create a list containing a single planet, that has default values.
+            List<GSPlanet> planets = new List<GSPlanet>
+            {
+                new GSPlanet("Urf")
+            };
+            planets[0].Radius = 5; //Lets make it tiny! (Vanilla size is 200).
+
+            //Create one O-type main sequence star, containing the above planet. Set the seed to one. This is the minimum requirement for a star. There are more options available.
+            GSSettings.Stars.Add(new GSStar(1, "BeetleJuice", ESpectrType.O, EStarType.MainSeqStar, planets)); 
+            //Create a whole bunch of identical empty F-type Giant stars
+            for (var i = 1; i < starCount; i++)
+            {
+                GSSettings.Stars.Add(new GSStar(1, "Star" + i.ToString(), ESpectrType.F, EStarType.GiantStar, new List<GSPlanet>()));
+            }
+
+            //Change some of the galaxy parameters. This is not necessary, but it lets this test generator actually do something meaningful to affect generation.
+            GSSettings.GalaxyParams.minDistance   = minDistance;
+            GSSettings.GalaxyParams.minStepLength = minStepLength;
+            GSSettings.GalaxyParams.maxStepLength = maxStepLength;
+        }
+    }
+}
